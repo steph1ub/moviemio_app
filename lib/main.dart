775b790bd0eadfb32aea27a58b1475584e3f27e9
STEPHANIE ELIZABTH UCAN BUSTILLO <stephanie.ucan@udgvirtual.udg.mx>
2026-05-17 07:59:35 -0500

    Integración de Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 //import 'package:moviemio_app/features/peliculas/presentation/pages/home_page.dart';
 import 'package:moviemio_app/features/peliculas/presentation/pages/welcome_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const MyApp(
+//void main() => runApp(const MyApp(
   
-));
+//));
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
